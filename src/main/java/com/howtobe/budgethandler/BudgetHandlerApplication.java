8319f9d44628916a9155b3fbc15ad98c5888054c
mirellaHalmai <halmaimirella@gmail.com>
2019-06-05 11:30:42 +0200

    Initial commit

---
+++ src/main/java/com/howtobe/budgethandler/BudgetHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.howtobe.budgethandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetHandlerApplication.class, args);
+    }
+
+}
